ed6d353af422c4209a5ffcf0b51101ff14e5142f
DANIEL VALDIVINO <ciadvsi@gmail.com>
2025-11-06 20:49:25 -0400

    #5 commit

--- desafios.py
+++ desafios.py
@@ -20,40 +20,27 @@ def mostrar_mensagem_inicial():
     
 
 def listar_comandos_git_basicos():
-    """
-    Retorna uma lista com os principais comandos básicos do Git.
-    Exemplo de saída:
-    ["git init", "git add", "git commit", "git status", "git push"]
-    """
-    pass
+    
+    return ["git init", "git add", "git commit", "git status", "git push"]
+    
+    
 
 
 def criar_mensagem_commit(funcao_nome):
-    """
-    Recebe o nome de uma função e retorna uma mensagem de commit padronizada.
-    Exemplo:
-    criar_mensagem_commit("listar_comandos_git_basicos") ->
-    "Implementa função listar_comandos_git_basicos"
-    """
-    pass
+    
+    return f"Implementa função {funcao_nome}"
 
 
 def verificar_tag_valida(tag):
-    """
-    Verifica se uma tag está no formato 'vX.Y' (ex: v1.0, v2.1).
-    Retorna True se o formato for válido, caso contrário False.
-    """
-    pass
+    
+    import re
+    return bool(re.fullmatch(r"v\d+\.\d+", tag))
 
 
 def gerar_relatorio_final(funcoes_concluidas):
-    """
-    Recebe uma lista com os nomes das funções implementadas
-    e retorna uma mensagem final do desafio.
 
-    Exemplo:
-    gerar_relatorio_final(["mostrar_mensagem_inicial", "listar_comandos_git_basicos"])
-    ->
-    "Desafio concluído! 2 funções implementadas com sucesso."
-    """
-    pass
+    
+    total_concluidas = len(funcoes_concluidas)
+    
+    
+    return f"Desafio concluído! {total_concluidas} funções implementadas com sucesso."
