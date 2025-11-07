b7f0590cccc49906c1b7ef4f088bc257d4587218
DANIEL VALDIVINO <ciadvsi@gmail.com>
2025-11-06 20:29:14 -0400

    #4 commit

--- desafios.py
+++ desafios.py
@@ -20,30 +20,21 @@ def mostrar_mensagem_inicial():
     
 
 def listar_comandos_git_basicos():
-    """
-    Retorna uma lista com os principais comandos básicos do Git.
-    Exemplo de saída:
-    ["git init", "git add", "git commit", "git status", "git push"]
-    """
-    pass
+    
+    return ["git init", "git add", "git commit", "git status", "git push"]
+    
+    
 
 
 def criar_mensagem_commit(funcao_nome):
-    """
-    Recebe o nome de uma função e retorna uma mensagem de commit padronizada.
-    Exemplo:
-    criar_mensagem_commit("listar_comandos_git_basicos") ->
-    "Implementa função listar_comandos_git_basicos"
-    """
-    pass
+    
+    return f"Implementa função {funcao_nome}"
 
 
 def verificar_tag_valida(tag):
-    """
-    Verifica se uma tag está no formato 'vX.Y' (ex: v1.0, v2.1).
-    Retorna True se o formato for válido, caso contrário False.
-    """
-    pass
+    
+    import re
+    return bool(re.fullmatch(r"v\d+\.\d+", tag))
 
 
 def gerar_relatorio_final(funcoes_concluidas):
